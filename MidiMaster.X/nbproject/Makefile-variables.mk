69617c1d0270008ee31a0338c88883b1fa5c0614
Masaharu41 <dalekrebel@gmail.com>
2024-11-05 09:08:12 -0700

    Added Master Assembly

---
+++ MidiMaster.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MidiMaster.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MidiMaster.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=midimaster.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/midimaster.x.tar
