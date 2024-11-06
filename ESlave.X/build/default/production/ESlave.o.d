eca66b4b0892fed4e2973a470ee77d9579a07602
Masaharu41 <dalekrebel@gmail.com>
2024-11-06 11:35:57 -0700

    added files for dummy string slave

---
+++ ESlave.X/build/default/production/ESlave.o.d
@@ -0,0 +1,6 @@
+build/default/production/ESlave.o:  \
+	C:/Users/jackel/Documents/Midi-Bass-Guitar/ESlave.X/ESlave.asm  \
+	C:/Users/jackel/Documents/Midi-Bass-Guitar/ESlave.X/ESlaveSetup.inc  \
+	C:/Users/jackel/Documents/Midi-Bass-Guitar/ESlave.X/ESlavePOPout.inc  \
+	C:/Users/jackel/Documents/Midi-Bass-Guitar/ESlave.X/ESlavePUSHin.inc  \
+
