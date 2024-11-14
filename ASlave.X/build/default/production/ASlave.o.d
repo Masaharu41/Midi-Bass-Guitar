267e705e41bc17807e023503523b9c89be6a9933
Masaharu41 <dalekrebel@gmail.com>
2024-11-14 14:14:58 -0700

    added dummy slave for A string

---
+++ ASlave.X/build/default/production/ASlave.o.d
@@ -0,0 +1,6 @@
+build/default/production/ASlave.o:  \
+	C:/Users/jackel/Documents/Midi-Bass-Guitar/ASlave.X/ASlave.asm  \
+	C:/Users/jackel/Documents/Midi-Bass-Guitar/ASlave.X/ASlaveSetup.inc  \
+	C:/Users/jackel/Documents/Midi-Bass-Guitar/ASlave.X/ASlavePOPout.inc  \
+	C:/Users/jackel/Documents/Midi-Bass-Guitar/ASlave.X/AslavePUSHin.inc  \
+
