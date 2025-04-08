705c35dafcb1951c560b8d92a2f29754fbc529b8
Masaharu41 <dalekrebel@gmail.com>
2025-04-07 20:27:10 -0600

    removed .x files

--- ESlave.X/build/default/production/ESlave.o.d
+++
@@ -1,6 +0,0 @@
-build/default/production/ESlave.o:  \
-	C:/Users/jackel/Documents/Midi-Bass-Guitar/ESlave.X/ESlave.asm  \
-	C:/Users/jackel/Documents/Midi-Bass-Guitar/ESlave.X/ESlaveSetup.inc  \
-	C:/Users/jackel/Documents/Midi-Bass-Guitar/ESlave.X/ESlavePOPout.inc  \
-	C:/Users/jackel/Documents/Midi-Bass-Guitar/ESlave.X/ESlavePUSHin.inc  \
-
--- PluckSlave.X/build/default/production/Pluck Slave.o.d
+++
@@ -1,6 +0,0 @@
-build/default/production/Pluck\ Slave.o:  \
-	C:/Users/jackel/Documents/Midi-Bass-Guitar/PluckSlave.X/Pluck\ Slave.asm  \
-	C:/Users/jackel/Documents/Midi-Bass-Guitar/PluckSlave.X/PluckSetup.inc  \
-	C:/Users/jackel/Documents/Midi-Bass-Guitar/PluckSlave.X/PluckPOPout.inc  \
-	C:/Users/jackel/Documents/Midi-Bass-Guitar/PluckSlave.X/PluckPUSHin.inc  \
-
